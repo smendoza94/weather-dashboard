a1b00fbe5dbea8c55a1d523b6d1174b4c39006d0
StevenM <stevenbmendoza@gmail.com>
2022-03-15 01:24:03 -0700

    site accepts user input and gets appropriate api data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,56 @@
+const userFormEl = $('#user-form');
+const cityInputEl = $('#cityName');
+const cityContainerEl = $('#city-weather-container');
+const citySearchTerm = $('#city-search-term')
+
+const popCityBtnEl = $('#popular-cities')
+
 const APIKey = `fac3e5205c2a5a41a3544c2e4efad7f2`;
 
-let cityName= `San Francisco`
-cityName = cityName.split(' ').join('+');
-// console.log(cityName);
+function formSubmitHandler(event) {
+    // prevent page from refreshing
+    event.preventDefault();
 
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${APIKey}`;
-// console.log(apiUrl);
+    // get value from input element
+    console.log(cityInputEl.val());
+    let cityName = cityInputEl.val().trim();
+    cityName = cityName.split(' ').join('+');
+    console.log(cityName);
 
-fetch(apiUrl).then(function(response) {
-    if (response.ok){
-        console.log(response);
-        response.json().then(function(data){
-            console.log(data);
-            // displayWeather(data, city);
-        });
+    if (cityName) {
+        getCityWeather(cityName);
+        // clear old content
+        cityContainerEl.text('');
+        cityInputEl.text('');
     } else {
-        alert('Error: ' + response.statusText);
+        alert('Please enter a city name')
     }
-}).catch(function(error){
-    alert('Unable to connect to OpenWeather Servers')
-})
+};
+
+function getCityWeather(city) {
+    // format the OpenWeather API URL
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`;
+    console.log(apiUrl);
+
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok){
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data);
+                displayWeather(data, city);
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
+    }).catch(function(error){
+        alert('Unable to connect to OpenWeather Servers')
+    })
+};
+
+function displayWeather(weather, searchTerm) {
+    
+};
+
+userFormEl.on('submit', formSubmitHandler);
